8c2689c2f0df64426c371ed47ff85706d932dadf
balor93 <e3l1i7e3>
2019-05-24 13:47:55 +0200

    file Chooser 2 FileUtils

--- app/src/main/java/com/j/filechooser2/FileUtils.java
+++ app/src/main/java/com/j/filechooser2/FileUtils.java
@@ -26,6 +26,7 @@ public class FileUtils {
                 if (cursor.moveToFirst()) {
                     Log.d("DDD", "Display Name: " + cursor.getString(column_index));
                     if(cursor.getString(column_index)==null){
+                       //to get filepath in th download
                         final String id = DocumentsContract.getDocumentId(uri);
                         final Uri contentUri = ContentUris.withAppendedId(
                                 Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
@@ -51,46 +52,6 @@ public class FileUtils {
         return null;
     }
 
-   /* public static void dumpImageMetaData(Context context,Uri uri) {
-
-        // The query, since it only applies to a single document, will only return
-        // one row. There's no need to filter, sort, or select fields, since we want
-        // all fields for one document.
-        Cursor cursor = context.getContentResolver()
-                .query(uri, null, null, null, null, null);
-
-        try {
-            // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
-            // "if there's anything to look at, look at it" conditionals.
-            if (cursor != null && cursor.moveToFirst()) {
-
-                // Note it's called "Display Name".  This is
-                // provider-specific, and might not necessarily be the file name.
-                String displayName = cursor.getString(
-                        cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                Log.d("DDD", "Display Name: " + displayName);
-
-                int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
-                // If the size is unknown, the value stored is null.  But since an
-                // int can't be null in Java, the behavior is implementation-specific,
-                // which is just a fancy term for "unpredictable".  So as
-                // a rule, check if it's null before assigning to an int.  This will
-                // happen often:  The storage API allows for remote files, whose
-                // size might not be locally known.
-                String size = null;
-                if (!cursor.isNull(sizeIndex)) {
-                    // Technically the column stores an int, but cursor.getString()
-                    // will do the conversion automatically.
-                    size = cursor.getString(sizeIndex);
-                } else {
-                    size = "Unknown";
-                }
-                Log.i("DD", "Size: " + size);
-            }
-        } finally {
-            cursor.close();
-        }
-    }*/
 
 
 
